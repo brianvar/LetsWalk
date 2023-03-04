7ef853ae15bfdc06b506e31977de989c9fbea645
Anton Serov <codeviolin@gmail.com>
2023-03-04 17:19:23 +0300

    Initial Commit

---
+++ LetsWalk/LetsWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsWalkApp.swift
+//  LetsWalk
+//
+//  Created by Anton Serov on 04.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LetsWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
